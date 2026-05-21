89fe41307f9cb17dafdf8a8095d9614e501b0010
ALEKSEYTASKIN95 <154746534+ALEKSEYTASKIN95@users.noreply.github.com>
2026-05-21 14:24:10 +0300

    add new class

--- src/main/java/org/javaguru/student_aleksey_taskin/lesson_1/HelloWorld.java
+++ src/main/java/org/javaguru/student_aleksey_taskin/lesson_1/HelloWorld.java
@@ -3,7 +3,24 @@ package org.javaguru.student_aleksey_taskin.lesson_1;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Aleksey");
+        System.out.println("Aleksey\"@\"");
         System.out.println("Taskin");
+    }
+}
+
+class Summa {
+
+    public static void main(String[] args) {
+        System.out.println(123 + 50);
+        System.out.println(120 / 2);
+    }
+}
+
+class Multiply {
+    public static void main(String[] args) {
+        int a = 80;
+        int b = 22;
+        int result = a * b;
+        System.out.println(result);
     }
 }
