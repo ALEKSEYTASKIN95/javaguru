cad36da06b175bcad362cad931f6c1296db5b06f
ALEKSEYTASKIN95 <154746534+ALEKSEYTASKIN95@users.noreply.github.com>
2026-05-21 10:58:31 +0300

    lesson_1 Aleksey Taskin System.out commit

---
+++ src/main/java/org/javaguru/student_aleksey_taskin/lesson_1/HelloWorld.java
@@ -0,0 +1,9 @@
+package org.javaguru.student_aleksey_taskin.lesson_1;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Aleksey");
+        System.out.println("Taskin");
+    }
+}
